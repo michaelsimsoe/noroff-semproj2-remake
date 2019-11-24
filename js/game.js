ff9b6142ef583caffb056cc145563e6de79ebb7a
Michael Krøysert-Simsø <mikke.korgen@gmail.com>
2019-11-25 00:42:47 +0100

    add animation to token move, start on dice functionality and style

--- js/game.js
+++ js/game.js
@@ -10,9 +10,11 @@ const gameTiles = [
   { x: 310, y: 50, traps: [] },
   { x: 300, y: 120, traps: [] },
   { x: 300, y: 200, traps: [] },
+  { x: 260, y: 250, traps: [] },
   { x: 220, y: 300, traps: [] },
   { x: 200, y: 400, traps: [] },
   { x: 180, y: 500, traps: [] },
+  { x: 170, y: 450, traps: [] },
   { x: 160, y: 600, traps: [] },
   { x: 160, y: 680, traps: [] },
   { x: 170, y: 750, traps: [] },
@@ -30,22 +32,55 @@ const gameTiles = [
 ];
 
 document.addEventListener('DOMContentLoaded', async function(event) {
-  let token = document.getElementById('player-token-one');
-  console.log(token.x.animVal.value);
-  console.log(token.x);
-  console.log(token.x.animVal.value);
-  console.log(token.x);
+  const TOKEN = document.getElementById('player-token-one');
+  const DICE_BTN = document.getElementById('dice-btn');
 
-  let count = 0;
-  setInterval(() => {
-    moveToken(token, gameTiles[count].x, gameTiles[count].y);
-    count += 1;
-    console.log(count);
-  }, 1000);
+  DICE_BTN.addEventListener('click', function(e) {
+    if (document.querySelector('.dice__side--active')) {
+      document
+        .querySelector('.dice__side--active')
+        .classList.remove('dice__side--active');
+    }
+    rollDiceAndMove(TOKEN);
+  });
 });
 
-function moveToken(token, x, y) {
-  token.setAttribute('x', x);
-  token.setAttribute('y', y);
+function moveToken(token, pos) {
+  if (!pos) return; // Return if there are no more moves left in gameTiles
+  token.setAttribute('x', pos.x);
+  token.setAttribute('y', pos.y);
   return;
+}
+
+function getRandomDiceResult() {
+  let randomNumberBetweenOneAndSix = Math.floor(Math.random() * 6) + 1;
+  return randomNumberBetweenOneAndSix;
+}
+
+function animateDice() {
+  const DICE_FIGURE = document.getElementById('dice-figure');
+  DICE_FIGURE.classList.add('dice__figure--active');
+}
+
+function diceNumberToString(num) {
+  return ['one', 'two', 'three', 'four', 'five', 'six'][num - 1];
+}
+
+function rollDiceAndMove(token) {
+  animateDice();
+  let moved = 1;
+  setTimeout(() => {
+    let moves = getRandomDiceResult();
+    document
+      .querySelector(`.dice__side-${diceNumberToString(moves)}`)
+      .classList.add('dice__side--active');
+    for (let i = 0; i < moves; i++) {
+      (function(i, moveCount) {
+        moved++;
+        setTimeout(() => {
+          moveToken(token, gameTiles[moveCount]);
+        }, 801 * i);
+      })(i, moved);
+    }
+  }, 2400);
 }
