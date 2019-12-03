a830d7c5e59b9d0c94fe1b7209980c6cd83a3d79
Michael Krøysert-Simsø <mikke.korgen@gmail.com>
2019-12-03 23:54:31 +0100

    Try again button styling

--- js/finale.js
+++ js/finale.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(e) {
+  const RETRY_BUTTON = document.getElementById('play-again-btn');
   const winnerHeading = document.getElementById('finale-sub-heading');
   const THRONE = document.querySelector('.throne > g > g');
   let winner = getWinner();
   winnerHeading.innerHTML = `${winner.name} of ${winner.house}`;
   setWinnerSigil(winner.house);
+  scrollToButton();
 
   function getWinner() {
     let fetchedWinner = localStorage.getItem('winner');
     let winner = JSON.parse(fetchedWinner);
     return winner.player;
+  }
+
+  function scrollToButton() {
+    setTimeout(() => {
+      RETRY_BUTTON.scrollIntoView();
+    }, 4000);
   }
 
   function setWinnerSigil(house) {
